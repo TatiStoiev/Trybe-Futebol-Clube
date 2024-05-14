61ea7431ecba370a2f624c7b5e4d28e3b53f5011
TatiStoiev <tatistoiev@hotmail.com>
2024-05-14 17:12:16 -0300

    refactor: improve TeamModel

--- app/backend/src/database/models/TeamModel.ts
+++ app/backend/src/database/models/TeamModel.ts
@@ -1,14 +1,17 @@
 import { ITeamModel } from '../../Interfaces/Teams/ITeamModel';
 import ITeams from '../../Interfaces/Teams/ITeams';
+import SequelizeTeams from './SequelizeTeams';
 
 export default class TeamModel implements ITeamModel {
+  private model = SequelizeTeams;
+
   async findAll(): Promise<ITeams[]> {
-    const response = await this.findAll();
-    return response.map(({ id, teamName }) => ({ id, teamName }));
+    const response = await this.model.findAll();
+    return response;
   }
 
   async findById(id: ITeams['id']): Promise<ITeams | null> {
-    const response = await this.findById(id);
+    const response = await this.model.findByPk(id);
     if (response === null) return null;
 
     const { teamName }: ITeams = response;

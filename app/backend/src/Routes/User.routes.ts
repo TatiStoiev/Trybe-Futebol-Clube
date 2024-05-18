3711a68a1b216e98669f1e91523e26d35a73a562
TatiStoiev <tatistoiev@hotmail.com>
2024-05-18 16:02:35 -0300

    feat: add router get login/role

--- app/backend/src/Routes/User.routes.ts
+++ app/backend/src/Routes/User.routes.ts
@@ -1,6 +1,7 @@
 import { Request, Router, Response } from 'express';
 import UserController from '../Controllers/User.controller';
-import Validations from '../middlewares/validations';
+import Validations, { CustomRequest } from '../middlewares/validations';
+import { TokenPayload } from '../types';
 
 const userController = new UserController();
 
@@ -10,6 +11,15 @@ UserRouter.post(
   '/',
   Validations.validateLogin,
   (req: Request, res: Response) => userController.login(req, res),
+);
+
+UserRouter.get(
+  '/role',
+  Validations.validateToken,
+  (req: CustomRequest, res: Response) => {
+    const userRole = (req.user as TokenPayload).role;
+    return res.status(200).json({ role: userRole });
+  },
 );
 
 export default UserRouter;

dac4ae973482e5825182cd1632b461fc457f7fd6
TatiStoiev <tatistoiev@hotmail.com>
2024-05-18 16:03:55 -0300

    refactor: replace JwtPayload to tokenpayload

--- app/backend/src/utils/JWT.ts
+++ app/backend/src/utils/JWT.ts
@@ -1,22 +1,29 @@
-import { JwtPayload, Secret, sign, SignOptions, verify } from 'jsonwebtoken';
+import { Secret, sign, SignOptions, verify } from 'jsonwebtoken';
+import { TokenPayload } from '../types';
+
+console.log('JWT_SECRET:', process.env.JWT_SECRET); // variavel de ambiente não está sendo reconhecida!!
 
 export default class JWT {
-  private static secret: Secret = process.env.JWT_SECRET || '';
+  private static secret: Secret = 'jwt_secret';
 
   private static jwtConfig: SignOptions = {
     expiresIn: '4h',
     algorithm: 'HS256',
   };
 
-  static sign(payload: JwtPayload): string {
+  static sign(payload: TokenPayload): string {
     return sign({ ...payload }, this.secret, this.jwtConfig);
   }
 
-  static verify(token: string): JwtPayload | string {
+  static async verify(token: string): Promise<string | TokenPayload> {
     try {
-      return verify(token, this.secret) as JwtPayload;
+      return verify(token, this.secret) as TokenPayload;
     } catch (error) {
       return 'Token must be a valid token';
     }
+  }
+
+  static extractToken(authorization: string) {
+    return authorization.split(' ')[1];
   }
 }

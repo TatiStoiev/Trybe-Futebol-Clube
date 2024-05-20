f15fcbb9e97fac11ed8d0edba53bf2c50f82ea2e
TatiStoiev <tatistoiev@hotmail.com>
2024-05-20 20:12:16 -0300

    feat: add router for search matches

--- app/backend/src/Routes/Match.routes.ts
+++ app/backend/src/Routes/Match.routes.ts
@@ -6,5 +6,7 @@ const matchController = new MatchController();
 const MatchRouter = Router();
 
 MatchRouter.get('/', (req: Request, res: Response) => matchController.findAllMatches(req, res));
+MatchRouter.get('/?', (req: Request, res: Response) =>
+  matchController.findMatchInProgress(req, res));
 
 export default MatchRouter;

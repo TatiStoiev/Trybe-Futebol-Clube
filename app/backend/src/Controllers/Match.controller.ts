406a57d3053dd74cb91babeaf025d2fa1ebe95a7
TatiStoiev <tatistoiev@hotmail.com>
2024-05-20 20:11:56 -0300

    feat: add findMatchInProgress

--- app/backend/src/Controllers/Match.controller.ts
+++ app/backend/src/Controllers/Match.controller.ts
@@ -11,4 +11,18 @@ export default class MatchController {
     const allMatches = await this.matchService.getAllMatches();
     return res.status(mapStatusHTTP(allMatches.status)).json(allMatches.data);
   }
+
+  public async findMatchInProgress(req: Request, res: Response) {
+    const InProgress = req.query.inProgress;
+
+    if (InProgress === 'true') {
+      const inProgressMatches = await this.matchService.getInProgressMatches(true);
+      return res.status(mapStatusHTTP(inProgressMatches.status)).json(inProgressMatches.data);
+    }
+
+    if (InProgress === 'false') {
+      const inProgressMatches = await this.matchService.getInProgressMatches(false);
+      return res.status(mapStatusHTTP(inProgressMatches.status)).json(inProgressMatches.data);
+    }
+  }
 }

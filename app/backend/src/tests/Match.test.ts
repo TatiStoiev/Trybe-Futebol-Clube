50b6dc63136df5bf1d4b76122e23763092bf2f11
TatiStoiev <tatistoiev@hotmail.com>
2024-05-20 20:56:54 -0300

    refactor: add more tests for search in progress matches

--- app/backend/src/tests/Match.test.ts
+++ app/backend/src/tests/Match.test.ts
@@ -7,7 +7,7 @@ import SequelizeMatch from '../database/models/matches/SequelizeMatch';
 import { app } from '../app';
 
 import MatchService from '../Services/Match.service';
-import { mockfindAllMatches, matchesInProgress } from './mocks/MatchMock';
+import { mockfindAllMatches, matchesInProgress, finishedMatches } from './mocks/MatchMock';
 
 chai.use(chaiHttp);
 
@@ -37,5 +37,14 @@ describe('Testes para a rota Match', () => {
 
     expect(response.status).to.be.equal(200);
     expect(response.body).to.be.deep.equal(matchesInProgress);
+  })
+
+  it('Deve retornar status SUCCESSFUL e a lista de partidas finalizadas se a query inProgress for false', async function () {
+    sinon.stub(matchService, 'getInProgressMatches').resolves(finishedMatches as any);
+
+    const response = await chai.request(app).get('/matches?inProgress=false');
+
+    expect(response.status).to.be.equal(200);
+    expect(response.body).to.be.deep.equal(finishedMatches);
   })
 });
--- app/backend/src/tests/mocks/MatchMock.ts
+++ app/backend/src/tests/mocks/MatchMock.ts
@@ -59,4 +59,35 @@ export const mockfindAllMatches = [
       }
     }
   ]
+
+  export const finishedMatches = [
+    {
+      "id": 1,
+      "homeTeamId": 16,
+      "homeTeamGoals": 1,
+      "awayTeamId": 8,
+      "awayTeamGoals": 1,
+      "inProgress": false,
+      "homeTeam": {
+        "teamName": "São Paulo"
+      },
+      "awayTeam": {
+        "teamName": "Grêmio"
+      }
+    },
+    {
+      "id": 2,
+      "homeTeamId": 9,
+      "homeTeamGoals": 1,
+      "awayTeamId": 14,
+      "awayTeamGoals": 1,
+      "inProgress": false,
+      "homeTeam": {
+        "teamName": "Internacional"
+      },
+      "awayTeam": {
+        "teamName": "Santos"
+      }
+    }
+  ]
    

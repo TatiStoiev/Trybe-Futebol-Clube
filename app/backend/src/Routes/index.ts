356ed2d0585439eba105b52432f46edc936f8a99
TatiStoiev <tatistoiev@hotmail.com>
2024-05-22 15:23:46 -0300

    feat: add leaderboarder route

--- app/backend/src/Routes/index.ts
+++ app/backend/src/Routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import TeamRouter from './Team.routes';
 import UserRouter from './User.routes';
 import MatchRouter from './Match.routes';
+import LeaderboardRouter from './Leaderboard.routes';
 
 const router = Router();
 
 router.use('/teams', TeamRouter);
 router.use('/login', UserRouter);
 router.use('/matches', MatchRouter);
+router.use('/leaderboard', LeaderboardRouter);
 
 export default router;
